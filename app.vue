26b13614de7030f5251335201c4e5e8cef2682fd
Oleksii Petrychenko <panleshapan@gmail.com>
2025-05-04 11:03:11 +0300

    app refactoring

--- app.vue
+++ app.vue
@@ -2,20 +2,7 @@
   <div class="bg-[#0d1117] text-white min-h-screen">
     <ScreenGuard v-if="tooSmall" />
     <template v-else>
-      <header
-        class="w-full flex items-center justify-between px-8 py-4 bg-[#0d1117] border-b border-gray-700 shadow-lg"
-      >
-        <NuxtLink
-          to="/"
-          class="text-2xl font-bold hover:text-blue-400 transition"
-        >
-          CryptoPulse by Oleksii
-        </NuxtLink>
-        <div class="flex items-center gap-4">
-          <LanguageSwitcher />
-        </div>
-      </header>
-
+      <SiteHeader />
       <CryptoTicker />
       <NuxtPage />
       <ChatBot />
